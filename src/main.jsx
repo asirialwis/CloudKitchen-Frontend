bdfc4532a27faa766cc9d840357cbb69fb190a7d
Christmith <it22328434@my.sliit.lk>
2025-04-26 01:17:15 +0530

    refactor main component to use MainRoute and ToastContainer with Suspense

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import MainRoute from "./routes/main-route";
+import Loader from "./components/common/loader";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Suspense fallback={<Loader />}>
+      <MainRoute />
+      <ToastContainer />
+    </Suspense>
+  </StrictMode>
+);
