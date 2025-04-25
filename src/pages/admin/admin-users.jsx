78dd84a07c6a40b963af8a6e3e5138df48e784a8
Christmith <it22328434@my.sliit.lk>
2025-04-26 01:18:14 +0530

    add AdminUsers component for managing admin users

---
+++ src/pages/admin/admin-users.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminUsers = () => {
+  return (
+    <div>
+      <h1>Admin Users</h1>
+      <p>Manage your admin users here.</p>
+    </div>
+  );
+};
+
+export default AdminUsers;
