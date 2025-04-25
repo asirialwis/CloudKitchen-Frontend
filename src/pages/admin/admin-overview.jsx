42967fa7d2f0f60e2489d60761e9bb83549f39be
Christmith <it22328434@my.sliit.lk>
2025-04-26 01:18:06 +0530

    add AdminOverview component for admin overview page

---
+++ src/pages/admin/admin-overview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminOverview = () => {
+  console.log("AdminOverview component is trying to render!");
+  return (
+    <div>
+      <h1>Admin Overview</h1>
+      <p>Welcome to the admin overview page.</p>
+    </div>
+  );
+};
+
+export default AdminOverview;
