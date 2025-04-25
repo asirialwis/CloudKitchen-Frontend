46f74debe00d42eac16cde8275c67432a7b55c21
Christmith <it22328434@my.sliit.lk>
2025-04-26 01:19:44 +0530

    add routing setup with lazy-loaded components for main, admin, and auth pages

---
+++ src/routes/paths.js
@@ -0,0 +1,54 @@
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+// Layouts
+const Main = lazy(() => import("./section/main"));
+const AdminMain = lazy(() => import("./section/admin-main"));
+
+// Main Pages
+const Home = lazy(() => import("../pages/main/home"));
+
+// Admin Pages
+const AdminOverview = lazy(() => import("../pages/admin/admin-overview"));
+const AdminUsers = lazy(() => import("../pages/admin/admin-users"));
+
+//Auth Pages
+import Register from "../pages/auth/register";
+import Login from "../pages/auth/login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Main,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    Component: AdminMain,
+    children: [
+      {
+        index: true,
+        Component: AdminOverview,
+      },
+      {
+        path: "users",
+        Component: AdminUsers,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    Component: Register,
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+]);
+
+export default router;
