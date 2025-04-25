1c714fbcf0d43aba23d71c0f23e60bfa4920cbd8
Christmith <it22328434@my.sliit.lk>
2025-04-26 01:17:51 +0530

    add Header component for site navigation

---
+++ src/layouts/main/header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">CloudKitchen</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="hover:text-gray-400">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-400">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-400">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
