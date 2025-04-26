b5f4ace965acaf237f0fcfc9d0f70d6ad70a812b
Christmith <it22328434@my.sliit.lk>
2025-04-26 10:31:28 +0530

    add Order component for the Order page

---
+++ src/pages/main/order.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Order = () => {
+  return (
+    <div>
+      <h1>Order Page</h1>
+      <p>Welcome to the Order page!</p>
+    </div>
+  );
+};
+
+export default Order;
