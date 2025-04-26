4caf967780687c9ede356b7bb2633e1aeb7d86ea
asirialwis <asirichathurika900@gmail.com>
2025-04-26 20:59:44 +0530

    logout api integrate

--- src/auth/auth.jsx
+++ src/auth/auth.jsx
@@ -1,5 +1,6 @@
 // src/auth/auth.js
 import { jwtDecode } from 'jwt-decode';
+import api from './api'; // Your configured axios instance with interceptors
 
 export const isAuthenticated = () => {
   const token = localStorage.getItem('accessToken');
@@ -26,6 +27,21 @@ export const logout = () => {
   localStorage.removeItem('refreshToken');
   window.location.href = '/login';
 };
+
+
+export const signOut = async () => {
+  try {
+    // Call backend signout endpoint
+    await api.post('/auth-service/user/signout');
+  } catch (error) {
+    console.error('Logout error:', error);
+  } finally {
+    // Always clear local tokens
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    window.location.href = '/login';
+  }
+}
 
 export const getAccessToken = () => {
   return localStorage.getItem('accessToken');
