e32b547ced0fd29de910ba6a9625f7302963bb79
Christmith <it22328434@my.sliit.lk>
2025-04-28 01:26:48 +0530

    refactor: update createCheckoutSession to use axios for API calls and include user ID in headers

--- src/api-calls/payment-API.js
+++ src/api-calls/payment-API.js
@@ -1,26 +1,72 @@
+// import stripePromise from "../stripe";
+// import { toast } from "react-toastify";
+
+// export const createCheckoutSession = async (cartItems) => {
+//   try {
+//     const stripe = await stripePromise;
+
+//     const response = await fetch(
+//       "http://localhost:3000/api/payment-service/create-checkout-session",
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(cartItems),
+//       }
+//     );
+
+//     if (!response.ok) {
+//       throw new Error("Failed to create checkout session");
+//     }
+
+//     const session = await response.json();
+
+//     const result = await stripe.redirectToCheckout({
+//       sessionId: session.id,
+//     });
+
+//     if (result.error) {
+//       console.error(result.error.message);
+//       toast.error(result.error.message, {
+//         position: "top-right",
+//         autoClose: 5000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         progress: undefined,
+//       });
+//     }
+//   } catch (error) {
+//     console.error("Checkout Error:", error.message);
+//     toast.error(error.message || "Something went wrong. Please try again!", {
+//       position: "top-right",
+//       autoClose: 5000,
+//     });
+//   }
+// };
+
 import stripePromise from "../stripe";
 import { toast } from "react-toastify";
+import api from "../auth/api";
 
 export const createCheckoutSession = async (cartItems) => {
   try {
     const stripe = await stripePromise;
 
-    const response = await fetch(
-      "http://localhost:3000/api/payment-service/create-checkout-session",
+    const response = await api.post(
+      "/payment-service/create-checkout-session",
+      cartItems,
       {
-        method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "x-user-id": localStorage.getItem("userId"),
         },
-        body: JSON.stringify(cartItems),
       }
     );
 
-    if (!response.ok) {
-      throw new Error("Failed to create checkout session");
-    }
-
-    const session = await response.json();
+    const session = response.data;
 
     const result = await stripe.redirectToCheckout({
       sessionId: session.id,
